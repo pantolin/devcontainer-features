1f3db0cd59927bd4cbe32a5c9b4a68436447811d
Pablo Antolin <pablo.antolin@epfl.ch>
2024-01-31 09:02:06 +0100

    Adding eza feature

---
+++ src/eza/README.md
@@ -0,0 +1,18 @@
+
+# Modern shell utils (modern-shell-utils)
+
+eza, A modern, maintained replacement for ls.
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/pantolin/devcontainer-features/eza:latest": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+
---
+++ src/eza/install.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Activating feature 'eza'"
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Step 1, check if user is root"
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+echo "Step 2, determine appropriate non-root user"
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} >/dev/null 2>&1; then
+    USERNAME=root
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Step 3, check if architecture is supported"
+architecture="$(uname -m)"
+if [ "${architecture}" != "amd64" ] && [ "${architecture}" != "x86_64" ] && [ "${architecture}" != "arm64" ] && [ "${architecture}" != "aarch64" ]; then
+    echo "(!) Architecture $architecture unsupported"
+    exit 1
+fi
+
+echo "Step 4, install required pgp public key"
+apt update -y
+apt install -y gpg
+mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
+echo "Step 5, install package"
+apt update -y
+apt install -y eza
+
+echo "Step 6, create symbolic link"
+ln -s /usr/bin/eza /usr/local/bin/eza
+
+
+echo "Step 7, clean up"
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
